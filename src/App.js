81313b4a725e5b1f468975de304733a990354829
Phillip Wong <deepelegant99@gmail.com>
2021-07-29 02:26:06 -0700

    make it more beauiful with less comments

--- src/App.js
+++ src/App.js
@@ -5,33 +5,13 @@ import React, { useEffect, useState } from "react";
 import Card from "./components/Card";
 
 function App() {
-  const topJobs = [
-    "Jave Developer",
-    "Data Scientist",
-    "Product Manager",
-    "Enterprise Architech",
-    "Devops Engineer",
-    "Information Security Engineer",
-    "Business Development Manager",
-    "Mobile Engineer",
-    "Software Engineer",
-    "Dentist",
-    "Front End Engineer",
-  ];
-
-  // image_url: "https://cdn.myanimelist.net/images/characters/11/286916.jpg?s=ff0b6e37a427c9498f5a1ecd40bf7bb4";
-  // mal_id: 720;
-  // name: "Liebert, Anna";
-  // role: "Main";
-  // url: "https://myanimelist.net/character/720/Anna_Liebert";
-
   const [names, setNames] = useState([]);
 
   useEffect(() => {
     fetch("https://api.jikan.moe/v3/manga/1/characters")
       .then((response) => response.json())
       .then((data) => {
-        setNames(data.characters)
+        setNames(data.characters);
       });
   }, []);
 
@@ -40,18 +20,6 @@ function App() {
       <header>
         <SearchBar />
         <DisplayList list={names} />
-        {/* <Card name={"Phillip"} role ={"Main"} url={"Some URL"} image_url={"Some image URL"} />  */}
-        {/* {names.map(
-          (dic)=>{
-            return (
-              <>
-                <div>{dic.name}</div>
-                <div>{dic.role}</div>
-                <div>{dic.url}</div>
-              </>
-            );
-          }
-        )} */}
       </header>
     </div>
   );
