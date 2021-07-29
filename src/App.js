b646b5f32796aec29d7cca201e84721b7c642678
Phillip Wong <deepelegant99@gmail.com>
2021-07-28 23:08:47 -0700

    output information about a anime character to screen

--- src/App.js
+++ src/App.js
@@ -18,17 +18,24 @@ function App() {
     "Front End Engineer",
   ];
 
+// image_url: "https://cdn.myanimelist.net/images/characters/11/286916.jpg?s=ff0b6e37a427c9498f5a1ecd40bf7bb4";
+// mal_id: 720;
+// name: "Liebert, Anna";
+// role: "Main";
+// url: "https://myanimelist.net/character/720/Anna_Liebert";
+
   const [names, setNames] = useState([]);
 
   useEffect(() => {
     fetch("https://api.jikan.moe/v3/manga/1/characters")
       .then((response) => response.json())
       .then((data) => {
-        setNames(
-          data.characters.map((item) => {
-            return item.name;
-          })
-        );
+        setNames(data.characters.map(
+          (dic)=>{
+            return <div>`name: ${dic.name} role: ${dic.role} image_url: 
+            ${dic.image_url} url: ${dic.url} `</div>
+          }
+        ))
       });
   }, []);
 
