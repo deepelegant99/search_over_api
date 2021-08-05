b68dde56a38869e83f88dff4584f5be42c9e793e
Henry Crossley <crossh2001@yahoo.com>
2021-08-04 21:50:40 -0700

    fix search

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Card from "./components/Card";
 
 function App() {
   const [names, setNames] = useState([]);
+  const [search, setSearch] = useState("");
+
 
   /* https://jikan.docs.apiary.io/#reference/0/search/meta-request-example+schema */
 
@@ -18,17 +20,24 @@ function App() {
   }, []);*/
 
   useEffect(() => {
-    fetch("https://api.jikan.moe/v3/search/anime?q=Dragon%Ball")
+console.log(`In search ${search}`);
+    if(search.length>0){
+      console.log(`In search ${search}`);
+    fetch(`https://api.jikan.moe/v3/search/anime?q=${search}`)
       .then((response) => response.json())
       .then((data) => {
         console.log(data.results);
       });
-  }, []);
+    }
+  }, [search]);
+
+  
+
 
   return (
     <div className="App">
       <header>
-        <SearchBar />
+        <SearchBar setSearch={setSearch} />
         <DisplayList list={names} />
       </header>
     </div>
